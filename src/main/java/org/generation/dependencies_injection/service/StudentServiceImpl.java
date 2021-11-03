0cafb19d427d7f098101d6f5497f919bfe160b6f
Hugo Bruno Perez Garcia <hugobruno737@gmail.com>
2021-11-03 14:58:51 -0600

    dependencies-injection

---
+++ src/main/java/org/generation/dependencies_injection/service/StudentServiceImpl.java
@@ -0,0 +1,45 @@
+package org.generation.dependencies_injection.service;
+
+import org.generation.dependencies_injection.model.Student;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private final List<Student> studentList = new ArrayList<>();
+
+    public StudentServiceImpl(){
+        addStudents();
+    }
+    @Override
+    public void add(Student student) {
+        studentList.add(student);
+
+    }
+
+    @Override
+    public void delete(Student student) {
+        studentList.remove(student);
+
+    }
+
+    @Override
+    public List<Student> all() {
+        return studentList;
+    }
+
+    @Override
+    public Student findById(String id) {
+        return studentList.get(Integer.parseInt(id));
+    }
+
+    private void addStudents(){
+        studentList.add(new Student("1", 1, "Laura", "Andres",1));
+        studentList.add(new Student("1", 1, "Laura", "Andres",1));
+        studentList.add(new Student("1", 1, "Laura", "Andres",1));
+
+    }
+}
